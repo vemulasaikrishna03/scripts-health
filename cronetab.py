ee81b820f685b339f3e6250fdaed33d36c9220fa
Saikrishna <vemulasaikrishna03@gmail.com>
2023-11-20 11:05:43 +0530

    added the cron jobs

--- cronetab.py
+++ cronetab.py
@@ -15,7 +15,12 @@ def remove_cron_job(command):
     cron.write()
 
 nginx_health_check = '/usr/bin/python3 /home/saikrishna/Desktop/scripts/scripts-health/nginx-_health.py >> /home/saikrishna/Desktop/scripts/scripts-health/output/nginx_output.txt 2>&1'
+uwsigi_health_check= '/usr/bin/python3 /home/saikrishna/Desktop/scripts/scripts-health/uwisgi-_health.py >> /home/saikrishna/Desktop/scripts/scripts-health/output/uwsigi_output.txt 2>&1'
+celery_health_check= '/usr/bin/python3 /home/saikrishna/Desktop/scripts/scripts-health/celery_health.py >> /home/saikrishna/Desktop/scripts/scripts-health/output/celery_output.txt 2>&1'
+celery_beat_health_check = '/usr/bin/python3 /home/saikrishna/Desktop/scripts/scripts-health/celery_beat_health.py >> /home/saikrishna/Desktop/scripts/scripts-health/output/output/celery_beat_output.txt 2>&1'
 
 
 #add_cron_job(nginx_health_check, '*/1 * * * *')
+#add_cron_job(uwsigi_health_check, '*/1 * * * *')
 remove_cron_job(nginx_health_check)
+remove_cron_job(uwsigi_health_check)
