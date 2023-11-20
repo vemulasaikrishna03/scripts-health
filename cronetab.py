df9720ce1da6cba083b3b807c3fefbb58fd77b3b
Saikrishna <vemulasaikrishna03@gmail.com>
2023-11-20 10:57:12 +0530

    added result capture

--- cronetab.py
+++ cronetab.py
@@ -14,8 +14,8 @@ def remove_cron_job(command):
             cron.remove(job)
     cron.write()
 
-nginx_health_check = '/usr/bin/python3 /home/saikrishna/Desktop/scripts/scripts-health/nginx-_health.py'
+nginx_health_check = '/usr/bin/python3 /home/saikrishna/Desktop/scripts/scripts-health/nginx-_health.py >> /home/saikrishna/Desktop/scripts/scripts-health/output/nginx_output.txt 2>&1'
 
 
-add_cron_job(nginx_health_check, '*/1 * * * *')
-#remove_cron_job(nginx_health_check)
+#add_cron_job(nginx_health_check, '*/1 * * * *')
+remove_cron_job(nginx_health_check)
